a2cb3534137f704ec9d9009d4035851709842ac6
Adina Valcauan <adina.valcauan@gmail.com>
2024-05-04 00:55:50 +0300

    Update App.js

--- FE/career-path/src/App.js
+++ FE/career-path/src/App.js
@@ -1,27 +1,33 @@
 import "./App.css";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
 import AdminMainPageWithRole from "./components/HigherOrderComponents/AdminMainPageWithRole";
 import UserMainPageWithRole from "./components/HigherOrderComponents/UserMainPageWithRole";
 import UserProfileWithRole from "./components/HigherOrderComponents/UserProfileWithRole";
 import ManageUsersAdminWithRole from "./components/HigherOrderComponents/ManageUsersAdminWithRole";
+import DiaryMainWithRole from "./components/HigherOrderComponents/DiaryMainWithRole";
+import DiaryDays from "./components/DiaryDays/DiaryDays";
+import UserContext from "./contexts/UserContext";
+import {useState} from "react";
+import LoginRegister from "./pages/LoginRegister/LoginRegister";
 
 function App() {
+    const [user, setUser] = useState(null);
     return (
+        <UserContext.Provider value={user}>
         <Router>
             <Routes>
-                <Route path="/" element={<Login/>}/>
-                {/*<Route path="/login" element={<Login/>}/>*/}
-                <Route path="/register" element={<Register/>}/>
+                <Route path="/" element={<LoginRegister/>}/>
                 <Route path="/mainpage" element={<UserMainPageWithRole/>}/>
                 <Route path="/users" element={<ManageUsersAdminWithRole/>}/>
                 <Route path="/profile" element={<UserProfileWithRole/>}/>
                 <Route path="/mainpageadmin" element={<AdminMainPageWithRole/>}/>
+                <Route path="/diary" element={<DiaryMainWithRole/>}/>
+                {/*<Route path="/diary" element={<DiaryDays/>}/>*/}
 
             </Routes>
         </Router>
+        </UserContext.Provider>
     );
 }
 
